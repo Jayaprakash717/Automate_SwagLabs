06d5e9f1adf4ab01ed2ddd6ae67d8b47c713b2c5
Jayaprakash717 <prakashjaya717@gmail.com>
2024-10-28 23:28:39 +0530

    First Commit

---
+++ Pages/SwagLabsPage.cs
@@ -0,0 +1,73 @@
+﻿using OpenQA.Selenium;
+
+public class SwagLabsPage
+{
+    private readonly IWebDriver _driver;
+
+    public SwagLabsPage(IWebDriver driver)
+    {
+        _driver = driver;
+    }
+
+    // Locators
+    private IWebElement UsernameField => _driver.FindElement(By.CssSelector("*[data-test=\"username\"]"));
+    private IWebElement PasswordField => _driver.FindElement(By.CssSelector("*[data-test=\"password\"]"));
+    private IWebElement LoginButton => _driver.FindElement(By.CssSelector("*[data-test=\"login-button\"]"));
+    private IWebElement CheckoutButton => _driver.FindElement(By.CssSelector("*[data-test=\"checkout\"]"));
+    private IWebElement FirstNameField => _driver.FindElement(By.CssSelector("*[data-test=\"firstName\"]"));
+    private IWebElement LastNameField => _driver.FindElement(By.CssSelector("*[data-test=\"lastName\"]"));
+    private IWebElement PostalCodeField => _driver.FindElement(By.CssSelector("*[data-test=\"postalCode\"]"));
+    private IWebElement ContinueButton => _driver.FindElement(By.CssSelector("*[data-test=\"continue\"]"));
+    private IWebElement FinishButton => _driver.FindElement(By.CssSelector("*[data-test=\"finish\"]"));
+    private IWebElement MenuButton => _driver.FindElement(By.CssSelector("#react-burger-menu-btn"));
+    private IWebElement LogoutLink => _driver.FindElement(By.CssSelector("*[data-test=\"logout-sidebar-link\"]"));
+
+    // Methods for actions
+    public void Login(string username, string password)
+    {
+        UsernameField.SendKeys(username);
+        PasswordField.SendKeys(password);
+        LoginButton.Click();
+    }
+
+    public void AddItemToCart(string item)
+    {
+        _driver.FindElement(By.CssSelector($"*[data-test=\"add-to-cart-{item}\"]")).Click();
+    }
+
+    public void RemoveItemFromCart(string item)
+    {
+        _driver.FindElement(By.CssSelector($"*[data-test=\"remove-{item}\"]")).Click();
+    }
+
+    public void GoToCart()
+    {
+        _driver.FindElement(By.CssSelector(".shopping_cart_link")).Click();
+    }
+
+
+    public void ProceedToCheckout()
+    {
+        CheckoutButton.Click();
+    }
+
+    public void EnterCheckoutInfo(string firstName, string lastName, string postalCode)
+    {
+        FirstNameField.SendKeys(firstName);
+        LastNameField.SendKeys(lastName);
+        PostalCodeField.SendKeys(postalCode);
+        ContinueButton.Click();
+    }
+
+    public void FinishCheckout()
+    {
+        FinishButton.Click();
+    }
+
+    public void Logout()
+    {
+        MenuButton.Click();
+        Thread.Sleep(TimeSpan.FromSeconds(3));
+        LogoutLink.Click();
+    }
+}
---
+++ SwagLabsTest.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Text.Json;
+
+namespace AutomationProject
+{
+    [TestFixture]
+    public class SwagLabsTest
+    {
+        private ChromeDriver driver;
+        private SwagLabsPage swagLabsPage;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            swagLabsPage = new SwagLabsPage(driver);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // Use Dispose to ensure proper cleanup
+            if (driver != null)
+            {
+                driver.Dispose();  // Dispose of the ChromeDriver instance
+            }
+        }
+
+
+        // Test method accepts input details as parameters
+        [Test]
+        [TestCase("standard_user", "secret_sauce", new[] { "sauce-labs-backpack", "sauce-labs-bike-light" }, new[] { "sauce-labs-backpack" }, "user", "lastname", "55555")]
+        public void SwagLabsTestScenario(string username, string password, string[] itemsToAdd, string[] itemsToRemove, string firstName, string lastName, string postalCode)
+        {
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            driver.Manage().Window.Maximize();
+
+            // Perform login
+            swagLabsPage.Login(username, password);
+
+            // Add items to cart
+            foreach (var item in itemsToAdd)
+            {
+                swagLabsPage.AddItemToCart(item);
+            }
+
+            // Remove items from cart
+            foreach (var item in itemsToRemove)
+            {
+                swagLabsPage.RemoveItemFromCart(item);
+            }
+
+            // Checkout process
+            swagLabsPage.GoToCart();
+            swagLabsPage.ProceedToCheckout();
+            swagLabsPage.EnterCheckoutInfo(firstName, lastName, postalCode);
+            swagLabsPage.FinishCheckout();
+
+            // Logout
+            swagLabsPage.Logout();
+        }
+    }
+}
